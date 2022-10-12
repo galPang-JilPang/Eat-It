a46d9ca788861348fc3183cecac2d4633212c64e
y00eunji <aine0227@naver.com>
2022-10-12 13:19:34 +0900

    feat: make simple HTML,JS in Home.js
    Home(투표목록)페이지 간단 html 및 js 작성

--- public/index.js
+++ public/index.js
@@ -1,21 +1,4 @@
-import { Home, Login } from './components/index.js';
-
-const $root = document.getElementById('root');
-const routes = [
-  { path: '/', component: Home },
-  { path: '/login', component: Login },
-];
-
-const render = async path => {
-  const _path = path ?? window.location.pathname;
-
-  try {
-    const component = routes.find(route => route.path === _path)?.component || NotFound;
-    $root.replaceChildren(await component());
-  } catch (err) {
-    console.error(err);
-  }
-};
+import render from './utils/render.js';
 
 window.addEventListener('popstate', () => {
   console.log('[popstate]', window.location.pathname);
