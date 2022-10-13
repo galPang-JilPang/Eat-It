35e53d8ef569496ef7d666814f6e2f636f887544
minkyung00 <mksin00@naver.com>
2022-10-13 09:11:21 +0900

    Feat: Implement firebase login and register
    firebase를 이용해 로그인과 회원가입 기능을 구현하였습니다.

--- public/utils/sign.js
+++ public/utils/sign.js
@@ -1,3 +1,6 @@
+import { auth } from './firebase.js';
+import render from '../index.js';
+
 // signin, signup에서 공통으로 사용하는 로직
 const signinState = {
   userid: {
@@ -21,13 +24,6 @@ const signinState = {
 
 const signupState = {
   ...signinState,
-  username: {
-    value: '',
-    get valid() {
-      return !!this.value;
-    },
-    error: '이름을 입력해 주세요.',
-  },
   'confirm-password': {
     value: '',
     get valid() {
@@ -43,20 +39,20 @@ let currentState = signinState;
 const toggleNav = e => {
   if (!e.target.matches('nav > ul > li')) return;
 
-  currentPage = currentPage === 'signin' ? 'signup' : 'signin';
-  currentState = currentState === signupState ? signinState : signupState;
+  const isSigninPage = e.target.classList.contains('signin');
+  currentPage = isSigninPage ? 'signin' : 'signup';
+  currentState = isSigninPage ? signinState : signupState;
 
   [...document.querySelectorAll('nav > ul > li')].forEach($li => $li.classList.toggle('active'));
 };
 
 const activeButton = () => {
-  const $siginForm = document.querySelector('.signin-form');
-  $siginForm.querySelector('button').disabled = !signinState.valid;
+  const $form = document.querySelector(`.${currentPage}-form`);
+  $form.querySelector('button').disabled = !signinState.valid;
 };
 
 const validate = e => {
   const { name, value } = e.target;
-  console.log(e.target);
   currentState[name].value = value.trim();
 
   // e.target.closest('.error-msg').textContent = !signinState[name].valid ? signinState[name].error : '';
@@ -64,7 +60,53 @@ const validate = e => {
   activeButton();
 };
 
+/* 로그인해야 접근할 수 있는 페이지에서 사용할 request */
+const authRequest = async () => {
+  const firebaseUserIdToken = await auth.currentUser?.getIdToken(true);
+
+  /**
+   * getIdToken(true): 사용자의 토큰을 refresh 해준다.
+   * 기본적으로, 1시간이 지나면 토큰은 만료된다.
+   * */
+
+  const response = await fetch('/api/hello', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer ' + firebaseUserIdToken,
+    },
+  });
+};
+
+const route = path => {
+  if (window.location.pathname === path) return;
+
+  window.history.pushState(null, null, path);
+  render(path);
+};
+
 const submit = async e => {
+  if (!e.target.matches('.signin-form') && !e.target.matches('.signup-form')) return;
+
+  e.preventDefault();
+
+  try {
+    if (currentPage === 'signin') {
+      const res = await auth.signInWithEmailAndPassword(signinState.userid.value, signinState.password.value);
+      route('/');
+    }
+
+    if (currentPage === 'signup') {
+      const res = await auth.createUserWithEmailAndPassword(signupState.userid.value, signupState.password.value);
+      route('/welcome');
+    }
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+/* express를 사용해서 구현한 방법 */
+const submitExpress = async e => {
   if (!e.target.matches('form')) return;
 
   e.preventDefault();
@@ -78,9 +120,9 @@ const submit = async e => {
       });
 
     const res = await axios.post(`/api/${currentPage}`, payload);
-    console.log(res);
     if (res.status === 200) {
       console.log(`${res.data.userid} 성공~!`);
+      route('/');
     }
   } catch (err) {
     console.error(err);
