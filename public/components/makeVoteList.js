2c78853a96cef4c75874ac0c0e41e9a06673bc61
codevil <namhundred@naver.com>
2022-10-14 19:40:38 +0900

    style: make better design
    지도 페이지 디자인 개선

--- public/components/makeVoteList.js
+++ public/components/makeVoteList.js
@@ -7,6 +7,8 @@ const makeVoteList = () => {
     '//dapi.kakao.com/v2/maps/sdk.js?appkey=c8627785e5fed8e94625831777adf1ea&libraries=services&autoload=false';
   document.head.appendChild(script);
   window.addEventListener('load', () => {
+    searchPlaces();
+
     window.addEventListener('submit', e => {
       e.preventDefault();
       searchPlaces();
@@ -14,22 +16,35 @@ const makeVoteList = () => {
   });
   return createElement(`
   <div class="map_wrap">
-      <div
-        id="map"
-        style="width: 100%; height: 100%; position: relative; overflow: hidden"
-      ></div>
+  <ul class="map-sidebar">
+    <li>
+      <button class="map-home">
+        지도 홈
+      </button>
+    </li>
+    <li>
+      <button class="map-list">
+        투표 목록
+      </button>
+    </li>
+    <li>
+      <button class="map-generate">
+        투표 생성
+      </button>
+    </li>
+  </ul>
+      <div id="map" ></div>
       <div id="menu_wrap" style="display: flex">
         <div id="menu_select" class="bg_white">
           <div class="option">
             <div>
               <form id="store-keyword">
-                키워드 :
-                <input type="text" id="keyword" size="15" />
-                <button type="submit">검색하기</button>
+                <input type="text" id="keyword" size="15" value="이태원"/>
+                <button type="submit"></button>
               </form>
             </div>
           </div>
-          <hr />
+
           <ul id="placesList"></ul>
           <div id="pagination"></div>
         </div>
