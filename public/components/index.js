d67e963361075ba14918d8a2134e5659343b2ef0
y00eunji <aine0227@naver.com>
2022-10-14 15:07:34 +0900

    feat: add MakeVoteList in render.js
    - MakeVoteList import 하기

--- public/components/index.js
+++ public/components/index.js
@@ -4,5 +4,6 @@ import Register from './register.js';
 import Welcome from './welcome.js';
 import Add from './add.js';
 import Voting from './voting.js';
+import MakeVoteList from './makeVoteList.js';
 
-export { Home, Login, Add, Voting, Register, Welcome };
+export { Home, Login, Add, Voting, Register, Welcome, MakeVoteList };
