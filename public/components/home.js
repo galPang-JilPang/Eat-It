f9fa0539183c55fcd565a096afea11ac3e6b1ec6
codevil <namhundred@naver.com>
2022-10-17 21:51:36 +0900

    refactor: update db constructor
    db 구조 개선

--- public/components/home.js
+++ public/components/home.js
@@ -11,13 +11,16 @@ const Home = async () => {
   `);
 
   const loginedEmail = localStorage.getItem('username');
+  const username = loginedEmail.split('@')[0];
 
-  const getVoteList = async () => {
-    const doc = await db.collection('users').doc(loginedEmail).collection('voteList').get();
-    console.log(doc);
-    const voteItems = doc.docs.map(voteItem => voteItem.data());
+  const getUserVoteList = async () => {
+    const docs = await db.collection('votes').orderBy('timestamp', 'desc').where('owner', '==', loginedEmail).get();
 
-    return voteItems.reverse();
+    let vote = [];
+    docs.forEach(doc => {
+      vote = [...vote, doc.data()];
+    });
+    return vote;
   };
 
   const isVoting = deadline => {
@@ -27,36 +30,34 @@ const Home = async () => {
     return voteDate > date;
   };
 
+  // prettier-ignore
   const fetchUserVoteList = voteItems => `
-
-    <div class="member-title">${loginedEmail.split('@')[0]}님의 투표 목록
+    <div class="member-title">${username}님의 투표 목록
      <a href="/add" class="add-vote"><img src="../src/plus.png"/></a>
    </div>
-
     <div class="vote-list-container">
-       ${voteItems
-         .map(
-           ({ id, title, deadline, stores }) => `
-           <div class="card" id="${id}">
-            <button class ="delete-vote">X</button>
-            <div class="vote-name">${title}</div>
-         <a href="/" class="vote-link">공유링크</a>
-        <div class="vote-date">${deadline}</div>
-        <div class="stores">
-        ${stores ? stores.map(store => `<span>${store}</span>`).join(' ') : ''}
-        </div>
-      <div class="card-status">
-        <div class="${isVoting(deadline) ? 'is-voting' : 'voted'}">${isVoting(deadline) ? '투표중' : '투표 완료'}</div>
-        <a href="${isVoting(deadline) ? '/makeVoteList' : '/voted'}" class="more-vote">더보기</a>
-      </div>
-    </div> 
-    `
-         )
-         .join('')}
+       ${voteItems.map(({ id, title, deadline, stores }) => `
+        <div class="card" id="${id}">
+          <button class ="delete-vote">X</button>
+          <div class="vote-name">
+            ${title}
+            <a href="/voting/:${id}" class="vote-link">공유링크</a>
+          </div>
+          <div class="vote-date">${deadline}</div>
+          <div class="stores">
+            ${stores ? stores.map(({ title }, index) => (index < 3 ? `<span>${title}</span>` : '')).join(' ') : ''}
+          </div>
+          <div class="card-status">
+            <div class="${isVoting(deadline) ? 'is-voting' : 'voted'}">${isVoting(deadline) ? '투표중' : '투표 완료'}</div>
+            <a href="${isVoting(deadline) ? `/voting/:${id}` : `/voted/:${id}`}" class="more-vote">더보기</a>
+          </div>
+        </div> 
+      `).join('')}
     </div>
     </div>
     `;
-  const voteItems = await getVoteList();
+
+  const voteItems = await getUserVoteList();
   const voteList = fetchUserVoteList(voteItems);
   const voteListElement = createElement(voteList);
 
@@ -65,12 +66,10 @@ const Home = async () => {
 };
 
 const $root = document.getElementById('root');
-
 // 투표 목록 추가 버튼
 $root.addEventListener('click', e => {
-  if (!e.target.closest('.add-vote')) return;
-
-  render(route(e));
+  if (e.target.closest('.add-vote')) render(route(e));
+  if (e.target.matches('.more-vote')) render(route(e));
 });
 
 // 투표 삭제
@@ -78,20 +77,11 @@ $root.addEventListener('click', async e => {
   if (!e.target.matches('.delete-vote')) return;
 
   const loginedEmail = localStorage.getItem('username');
-  const targetId = +e.target.closest('.card').id;
-  const doc = await db.collection('users').doc(loginedEmail).collection('voteList').where('id', '==', targetId).get();
+  const targetId = e.target.closest('.card').id;
 
-  doc.forEach(element => {
-    element.ref.delete();
-  });
+  db.collection('votes').doc(targetId).delete();
 
   render();
-});
-
-$root.addEventListener('click', e => {
-  if (!e.target.matches('.more-vote')) return;
-
-  render(route(e));
 });
 
 export default Home;
--- public/components/makeVoteList.js
+++ public/components/makeVoteList.js
@@ -1,57 +1,158 @@
 import createElement from '../utils/createElement.js';
+import { db } from '../utils/firebase.js';
 import { searchPlaces } from '../utils/kakaomap.js';
+import route from '../utils/route.js';
+import render from '../utils/render.js';
+import renderSelectedStoreList from './renderSelectedStoreList.js';
 
-const makeVoteList = () => {
-  const script = document.createElement('script');
-  script.src =
-    '//dapi.kakao.com/v2/maps/sdk.js?appkey=c8627785e5fed8e94625831777adf1ea&libraries=services&autoload=false';
-  document.head.appendChild(script);
+const makeVoteList = params => {
+  let selectedStoreList = [];
+
   window.addEventListener('load', () => {
-    searchPlaces();
-
+    // searchPlaces();
     window.addEventListener('submit', e => {
+      if (!e.target.matches('#store-keyword')) return;
       e.preventDefault();
       searchPlaces();
     });
   });
-  return createElement(`
-  <div class="map_wrap">
-  <ul class="map-sidebar">
-    <li>
-      <button class="map-home">
-        지도 홈
-      </button>
-    </li>
-    <li>
-      <button class="map-list">
-        투표 목록
-      </button>
-    </li>
-    <li>
-      <button class="map-generate">
-        투표 생성
-      </button>
-    </li>
-  </ul>
-      <div id="map" ></div>
-      <div id="menu_wrap" style="display: flex">
-        <div id="menu_select" class="bg_white">
-          <div class="option">
-            <div>
-              <form id="store-keyword">
-                <input type="text" id="keyword" size="15" value="이태원"/>
-                <button type="submit"></button>
-              </form>
-            </div>
-          </div>
 
-          <ul id="placesList"></ul>
-          <div id="pagination"></div>
+  window.addEventListener('click', async e => {
+    if (!e.target.matches('.add-store')) return;
+
+    const $store = e.target.closest('li');
+    const selectedStore = {
+      countVote: 0,
+      id: $store.className.split('-')[1],
+      title: $store.querySelector('.store-name').textContent,
+      description: $store.querySelector('.store-description').textContent,
+      tel: $store.querySelector('.tel').textContent,
+      thumbnails: [...$store.querySelectorAll('.store-image')].map($store =>
+        $store.style.backgroundImage.slice(4, -1).replace(/"/g, '')
+      ),
+    };
+
+    selectedStoreList = [...selectedStoreList, selectedStore];
+
+    e.target.disabled = true;
+  });
+
+  window.addEventListener('click', async e => {
+    if (e.target.matches('.map-home')) {
+      document.querySelector('#menu_voted').style.display = 'none';
+      document.querySelector('#menu_wrap').style.display = 'block';
+      renderSelectedStoreList(selectedStoreList);
+    }
+    if (e.target.matches('.map-list')) {
+      document.querySelector('#menu_voted').style.display = 'block';
+      document.querySelector('#menu_wrap').style.display = 'none';
+
+      renderSelectedStoreList(selectedStoreList);
+    }
+    if (e.target.matches('.remove-btn')) {
+      selectedStoreList = selectedStoreList.filter(store => store.id !== e.target.closest('li').id);
+      renderSelectedStoreList(selectedStoreList);
+
+      const targetId = e.target.closest('li').id;
+      document.querySelector(`li.item-${targetId}`).querySelector('.add-store').disabled = false;
+    }
+    if (e.target.matches('.total-submit-btn')) {
+      e.preventDefault();
+      const voteItem = await db.collection('votes').doc(params);
+      voteItem.update({ stores: firebase.firestore.FieldValue.arrayUnion(...selectedStoreList) });
+      render(route(e));
+    }
+  });
+  // window.addEventListener('beforeunload', () => {
+  //   console.log(db.collection('votes').doc(params));
+  // if (isSubmitted) return;
+  // db.collection('votes').doc(params).delete();
+  // });
+  // window.addEventListener('click', e => {
+  //   if (!e.target.matches('.map-home')) return;
+
+  //   document.querySelector('#store-detail').style.display = 'none';
+  //   document.querySelector('#menu_select').style.display = 'block';
+
+  //   renderSelectedStoreList();
+  // });
+
+  // window.addEventListener('click', e => {
+  //   if (!e.target.matches('.map-list')) return;
+
+  //   document.querySelector('#store-detail').style.display = 'block';
+  //   document.querySelector('#menu_select').style.display = 'none';
+
+  //   renderSelectedStoreList();
+  // });
+
+  // window.addEventListener('click', e => {
+  //   if (!e.target.matches('.remove-btn')) return;
+  //   selectedStoreList = selectedStoreList.filter(store => store.id !== e.target.closest('li').id);
+
+  //   renderSelectedStoreList();
+  // });
+
+  // window.addEventListener('click', async e => {
+  //   if (!e.target.matches('.total-submit-btn')) return;
+
+  //   e.preventDefault();
+
+  //   const voteItem = await db
+  //     .collection('votes')
+  //     .where('id', '==', params)
+  //     .update({ stores: firebase.firestore.FieldValue.arrayUnion(...selectedStoreList) });
+
+  // const uuid = await db.collection('votes').doc();
+
+  // uuid.set({
+  //   stores: firebase.firestore.FieldValue.arrayUnion(...selectedStoreList),
+  //   owner: user,
+  //   id: uuid.id,
+  // });
+
+  // db.collection('users').doc(user).collection('voteList').doc(uuid.id).set({});
+
+  // render(route(e));
+  // });
+
+  return createElement(`
+    <div class="map_wrap">
+    <ul class="map-sidebar">
+      <li>
+        <button class="map-home">
+          지도 홈
+        </button>
+      </li>
+      <li>
+        <button class="map-list">
+          투표 목록
+        </button>
+      </li>
+      <li>
+      <a href="/home" class="total-submit-btn">투표 완료</a>
+
+      </li>
+    </ul>
+        <div id="map" ></div>
+        <div id="menu_wrap" style="display: flex">
+          <div id="menu_select" class="bg_white">
+            <div class="option">
+              <div>
+                <form id="store-keyword">
+                  <input type="text" id="keyword" size="15" value="강남역 맛집"/>
+                  <button type="submit"></button>
+                </form>
+              </div>
+            </div>
+
+            <ul id="placesList"></ul>
+            <div id="pagination"></div>
+          </div>
+          <div id="store-detail"></div>
         </div>
-        <div id="store-detail"></div>
+        <div id="menu_voted" style="display:none;"></div>
       </div>
-    </div>
-    
 `);
 };
 
