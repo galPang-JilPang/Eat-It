9fc0b6ce19b517ccce3b48b778d004234133451f
minkyung00 <mksin00@naver.com>
2022-10-17 10:25:00 +0900

    fix: render error
    새로고침, 뒤로가기, 앞으로 가기 버튼을 눌렀을 때 제대로 동작하지 않는 렌더 문제를 해결하였습니다.

--- public/index.js
+++ public/index.js
@@ -2,11 +2,15 @@ import render from './utils/render.js';
 
 window.addEventListener('popstate', () => {
   console.log('[popstate]', window.location.pathname);
-  render();
+  const [path, params] = window.location.pathname.split('/:');
+
+  render({ path, params });
 });
 
 window.addEventListener('DOMContentLoaded', () => {
-  render();
+  const [path, params] = window.location.pathname.split('/:');
+
+  render({ path, params });
 });
 
 export default render;
