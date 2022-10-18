4a00259b9dd753794c4e5e2282c10977dd1fedf4
minkyung00 <mksin00@naver.com>
2022-10-18 17:05:31 +0900

    fix: set navigation auth
    로그인하지 않은 사용자의 경우 네비게이션이 보이지 않도록 수정하였습니다.

--- public/components/nav.js
+++ public/components/nav.js
@@ -2,22 +2,25 @@ import render from '../utils/render.js';
 import route from '../utils/route.js';
 import { logout, logoutExpress } from '../utils/sign.js';
 
-const Nav = () => `
-  <nav>
-    <ul>
-      <li><a href="/">투표 목록</a></li>
-      <li><a href="/add">투표 추가</a></li>
-      <li class="user">${localStorage.getItem('username')}</li>
-      ${
-        localStorage.getItem('username')
-          ? `<li class="logout">
-              <a href="/login">로그아웃</a>
-            </li>`
-          : ''
-      }
-    </ul>
-  </nav>
-`;
+const Nav = () => {
+  return window.localStorage.getItem('username')
+    ? `<nav>
+      <ul>
+        <li><a href="/">투표 목록</a></li>
+        <li><a href="/add">투표 추가</a></li>
+        <li class="user">${localStorage.getItem('username')}</li>
+        ${
+          localStorage.getItem('username')
+            ? `<li class="logout">
+                <a href="/login">로그아웃</a>
+              </li>`
+            : ''
+        }
+      </ul>
+    </nav>
+  `
+    : `<nav>방문자 투표입니다</nav>`;
+};
 
 window.addEventListener('click', e => {
   if (!e.target.matches('nav > ul > li > a')) return;
